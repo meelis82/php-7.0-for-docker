371933b585d0c8034da390639fec3b010599253c
Meelis Valgeväli <meelis@adm.ee>
2019-01-09 14:25:55 +0200

    2.7.1beta1

--- src/php-7.0/build/modules/build/manifests/php70/extensions/xdebug.pp
+++ src/php-7.0/build/modules/build/manifests/php70/extensions/xdebug.pp
@@ -1,31 +1,31 @@
 class build::php70::extensions::xdebug {
   require build::php70
 
-  file { '/tmp/xdebug-2.6.1.tgz':
+  file { '/tmp/xdebug-2.7.0beta1.tgz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/xdebug-2.6.1.tgz'
+    source => 'puppet:///modules/build/tmp/xdebug-2.7.0beta1.tgz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf xdebug-2.6.1.tgz':
-    require => File['/tmp/xdebug-2.6.1.tgz']
+  bash_exec { 'cd /tmp && tar xzf xdebug-2.7.0beta1.tgz':
+    require => File['/tmp/xdebug-2.7.0beta1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && phpize-7.3.0':
-    require => Bash_exec['cd /tmp && tar xzf xdebug-2.6.1.tgz']
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && phpize-7.3.0':
+    require => Bash_exec['cd /tmp && tar xzf xdebug-2.7.0beta1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.0':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.0':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && phpize-7.3.0']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && phpize-7.3.0']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && make':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.0']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.0']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && make install':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && make install':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && make']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && make']
   }
 }
